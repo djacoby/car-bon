a9abc2897ebb6b1b346f904825927734697a9569
David Jacoby <jacoby934@gmail.com>
2022-11-08 08:59:57 -0500

    WIP lookup vehicle without trim

--- pages/api/vehicle/trims.ts
+++ pages/api/vehicle/trims.ts
@@ -5,7 +5,7 @@ import { reshapeApiResponse } from '../../../shared/reshape-api-response';
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ApiResponse<FuelEconomyApiValue[]>>
+  res: NextApiResponse<ApiResponse<FuelEconomyApiValue | FuelEconomyApiValue[]>>
 ) {
     if (req.method !== 'GET') {
       return res.status(405).json({ result: 'Method not allowed', error: true })    
@@ -24,9 +24,41 @@ export default async function handler(
       }
     })
 
+    if (trims.status !== 200) {
+      lookupVehicleWithoutTrim(req, res)
+    }
+
     const response: FuelEconomyApiResponse = await trims.json()
 
     const result = reshapeApiResponse(response)
 
     res.status(200).json({ result, error: false })
+}
+
+async function lookupVehicleWithoutTrim(
+  req: NextApiRequest,
+  res: NextApiResponse<ApiResponse<FuelEconomyApiValue | FuelEconomyApiValue[]>>,
+) {
+  const { year, make, model } = req.query;
+
+  const vehicle = await fetch(`https://www.fueleconomy.gov/feg/PowerSearch.do?action=noform&path=1&year1=${year}&year2=${year}&make=${make}&baseModel=${model}&srchtyp=ymm&pageno=1&rowLimit=50`, {
+    method: 'GET',
+  })
+
+  if (vehicle.status !== 200) {
+    res.status(500).json({ result: 'No vehicle found', error: true})
+  }
+
+  const response = await vehicle.text()
+  const startIndex = response.search('myid="') + 6
+  const endIndex = startIndex + 5
+
+  const vehicleId = response.slice(startIndex, endIndex)
+
+  const result: FuelEconomyApiValue = {
+    label: '',
+    value: vehicleId,
+  }
+
+  res.status(200).json({ result, error: false })
 }
