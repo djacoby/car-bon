95d0a2c5f3aa8b968de3a60d5d78bacce7227111
djacoby <djacoby@sensourceinc.com>
2022-11-10 16:02:27 -0500

    add geo coords to starting point for map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,14 +1,32 @@
 import { useEffect } from 'react';
 import { Map } from 'maplibre-gl'
 
+const getUserLocationErrorCb = (error: any) => {
+  console.log(error)
+}
+
 export default function DropdownSelect() {
   useEffect(() => {
-    new Map({
-    container: 'map',
-    style: 'https://tiles.stadiamaps.com/styles/osm_bright.json', // stylesheet location
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
-  });
+    // TODO: refactor this and add some sort of loader if fetching geographic coords
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        new Map({
+          container: 'map',
+          style: 'https://tiles.stadiamaps.com/styles/osm_bright.json', // stylesheet location
+          center: [position.coords.longitude, position.coords.latitude], // starting position [lng, lat]
+          zoom: 10, // starting zoom
+        });
+      },
+      () => {
+        new Map({
+          container: 'map',
+          style: 'https://tiles.stadiamaps.com/styles/osm_bright.json', // stylesheet location
+          center: [-74.0060, 40.7128], // starting position [lng, lat]
+          zoom: 9, // starting zoom
+        });
+      }
+    )
+
   }, [])
 
   return (
