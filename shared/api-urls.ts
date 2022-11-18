d1db545e7c70b93b4f9c90109a6f918239372177
David Jacoby <jacoby934@gmail.com>
2022-11-17 23:17:58 -0500

    extract 3rd party api url & routes to env & config

---
+++ shared/api-urls.ts
@@ -0,0 +1,17 @@
+import { config } from './config';
+
+/**
+ * Fuel Economy API URL
+ */
+export const fuelEconomyApiRoute = {
+  year: () => `${config.fuelEconomyApiUrl}/menu/year`,
+  make: (year: string) => `${config.fuelEconomyApiUrl}/menu/make?year=${year}`,
+  model: (year: string, make: string) => `${config.fuelEconomyApiUrl}/menu/model?year=${year}&make=${make}`,
+  trim: (year: string, make: string, model: string) => `${config.fuelEconomyApiUrl}/menu/options?year=${year}&make=${make}&model=${model}`,
+  vehicle: (id: string) => `${config.fuelEconomyApiUrl}/${id}`,
+};
+
+/**
+ * Open cage API URL
+ */
+export const openCageApiUrl = `${config.openCageApiUrl}${config.openCageApiKey}`;
